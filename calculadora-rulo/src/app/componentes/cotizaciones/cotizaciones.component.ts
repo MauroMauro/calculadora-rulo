48c3892bedc77ca19965c6de7389c7f0fff25ad4
Blur <blur.benitez@gmail.com>
2021-01-18 19:57:37 -0300

    Agregados las consultas a la api

--- calculadora-rulo/src/app/componentes/cotizaciones/cotizaciones.component.ts
+++ calculadora-rulo/src/app/componentes/cotizaciones/cotizaciones.component.ts
@@ -10,6 +10,34 @@ export class CotizacionesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
+    var bluediv = document.getElementById("blue");
+    var mepdiv = document.getElementById("mep");
+    var oficialdiv = document.getElementById("oficial");
+    var solidariodiv = document.getElementById("solidario");
+
+    fetch('https://criptoya.com/api/dolar')
+      .then(response => response.json())
+      .then(json => bluediv.innerHTML = json.blue )
+      .then(json => console.log(json))
+
+    fetch('https://criptoya.com/api/dolar')
+      .then(response => response.json())
+      .then(json => mepdiv.innerHTML = json.mep )
+      .then(json => console.log(json))
+
+    fetch('https://criptoya.com/api/dolar')
+      .then(response => response.json())
+      .then(json => oficialdiv.innerHTML = json.oficial )
+      .then(json => console.log(json))
+
+    fetch('https://criptoya.com/api/dolar')
+      .then(response => response.json())
+      .then(json => solidariodiv.innerHTML = json.solidario )
+      .then(json => console.log(json))
+
+
+
   }
 
 }
--- calculadora-rulo/src/app/componentes/panel-rulo/panel-rulo.component.ts
+++ calculadora-rulo/src/app/componentes/panel-rulo/panel-rulo.component.ts
@@ -4,24 +4,65 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-panel-rulo',
   templateUrl: './panel-rulo.component.html',
   styleUrls: ['./panel-rulo.component.css']
-})
+}) 
 export class PanelRuloComponent implements OnInit {
 
-  cotizacionOficial: number = 80;
-  inversionUsd: number = 200;
-  inversionArs: number = this.inversionUsd * this.cotizacionOficial;
-  impuestos: number = 65;
-  cotizacion: number = this.cotizacionOficial + ((this.cotizacionOficial * this.impuestos)/100);
-  cotizacionBlue: number = 160;
+
+  cotizacionOficial: number;
+  inversionUsd: number;
+  inversionArs: number;
+  impuestos: number;
+  cotizacion: number;
+  cotizacionBlue: number;
 
   //la "diferenciaBlueSobreOficial" refleja el porcentaje de ventaja del Blue sobre la "cotizacion"
-  diferenciaBlueSobreOficial:number = ((this.cotizacionBlue - this.cotizacion) * 100)/ this.cotizacionBlue;
+  diferenciaBlueSobreOficial:number;
 
   constructor() { }
 
 
 
   ngOnInit(): void {
+
+    this.definirCotizaciones();
+    this.definirMontos();
   }
 
+  definirCotizaciones(){
+
+    fetch('https://criptoya.com/api/dolar')
+      .then(response => response.json())
+      .then(json => this.cotizacionOficial = json.oficial )
+      .then(json => console.log('cotizacion oficial: '+json))
+
+    fetch('https://criptoya.com/api/dolar')
+      .then(response => response.json())
+      .then(json => this.inversionArs = (this.inversionUsd * json.oficial) )
+      .then(json => console.log('inversion ars: '+json))
+
+    fetch('https://criptoya.com/api/dolar')
+      .then(response => response.json())
+      .then(json => this.cotizacionBlue = json.blue )
+      .then(json => console.log('cotizacion blue: '+json))
+  }
+  
+
+  definirMontos() {
+    
+    console.log("en definir montos, la cotizacion blue: " + this.cotizacionBlue);
+    console.log("en definir montos, la cotizacion oficial: " + this.cotizacionOficial);
+    console.log("en definir montos, los impuestos: " + this.impuestos);
+    this.inversionUsd = 200;
+    this.inversionArs = 0;
+    this.impuestos = 65;
+    //this.cotizacion = this.cotizacionOficial + ((this.cotizacionOficial * this.impuestos) / 100);
+    //console.log("cotizacion: " + this.cotizacion);
+    
+    //la "diferenciaBlueSobreOficial" refleja el porcentaje de ventaja del Blue sobre la "cotizacion"
+    //this.diferenciaBlueSobreOficial = ((this.cotizacionBlue - this.cotizacion) * 100) / this.cotizacionBlue;
+
+
+  }
+
+  
 }
